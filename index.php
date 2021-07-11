8e75b6ed862ea20410ff2d91f4ea8f6614fb51a7
Lukman <61648308+Lukman350@users.noreply.github.com>
2021-07-11 14:31:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $data = getCurl("https://samp-api.tk/server/YOUR_SERVER_IP_HERE");
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
 
-    <link href="assets/css/theme.css" rel="stylesheet" />
+    <link href="assets/css/theme.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="assets/css/landing/style.css">
 
   </head>
